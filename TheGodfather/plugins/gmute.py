b21a6b9f2c3ae2ac0dc45bd0e27dc3f099c6eb49
Zaid <itzzaid@pm.me>
2022-07-30 10:15:41 +0530

    Create gmute.py

---
+++ TheGodfather/plugins/gmute.py
@@ -0,0 +1,53 @@
+from config import PREFIX
+
+from pyrogram import filters, Client
+
+from TheGodfather.database.gmutedb import get_gmuted_users, gmute_user, ungmute_user
+from TheGodfather.helpers.pyrohelper import get_arg
+from TheGodfather.helpers.adminhelpers import CheckAdmin
+
+
+@Client.on_message(filters.command("gmute", PREFIX) & filters.me)
+async def gmute(app: Client, message):
+    reply = message.reply_to_message
+    if reply:
+        user = reply.from_user["id"]
+    else:
+        user = get_arg(message)
+        if not user:
+            await message.edit("**Whome should I gmute?**")
+            return
+    get_user = await app.get_users(user)
+    await gmute_user(get_user.id)
+    await message.edit(f"**Gmuted {get_user.first_name}, LOL!**")
+
+
+@Client.on_message(filters.command("ungmute", PREFIX) & filters.me)
+async def gmute(app: Client, message):
+    reply = message.reply_to_message
+    if reply:
+        user = reply.from_user["id"]
+    else:
+        user = get_arg(message)
+        if not user:
+            await message.edit("**Whome should I ungmute?**")
+            return
+    get_user = await app.get_users(user)
+    await ungmute_user(get_user.id)
+    await message.edit(f"**Unmuted {get_user.first_name}, enjoy!**")
+
+
+@Client.on_message(filters.group & filters.incoming)
+async def check_and_del(app: Client, message):
+    if not message:
+        return
+    try:
+        if not message.from_user.id in (await get_gmuted_users()):
+            return
+    except AttributeError:
+        return
+    message_id = message.message_id
+    try:
+        await app.delete_messages(message.chat.id, message_id)
+    except:
+        pass
