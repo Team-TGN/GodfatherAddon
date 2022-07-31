ed43973db57d07e2a8c6156b1b34e63b04747b1f
☆*:.｡.ⲘⴑⲔⴑⲚⲆ.｡.:*☆ <98509058+Legend-Mukund@users.noreply.github.com>
2022-07-31 21:05:59 +0530

    Update whois.py

--- TheGodfather/plugins/whois.py
+++ TheGodfather/plugins/whois.py
@@ -41,24 +41,4 @@ def FullName(user: User):
 
 
 
-@Client.on_message(filters.command("id", PREFIX) & filters.me)
-async def id(app: Client, message):
-    cmd = message.command
-    chat_id = message.chat.id
-    if not message.reply_to_message and len(cmd) == 1:
-        get_user = message.from_user.id
-    elif len(cmd) == 1:
-        get_user = message.reply_to_message.from_user.id
-    elif len(cmd) > 1:
-        get_user = cmd[1]
-        try:
-            get_user = int(cmd[1])
-        except ValueError:
-            pass
-    try:
-        user = await app.get_users(get_user)
-    except PeerIdInvalid:
-        await message.edit("I don't know that User.")
-        return
-    text = "**User ID**: `{}`\n**Chat ID**: `{}`".format(user.id, chat_id)
-    await message.edit(text)
+
