9441031240649c596aba0ae55f0274be2b2147c0
Zaid <itzzaid@pm.me>
2022-07-31 08:06:19 +0530

    Update alive.py

--- TheGodfather/plugins/alive.py
+++ TheGodfather/plugins/alive.py
@@ -60,9 +60,7 @@ async def alive(app: Client, m):
     reply_msg += f"Python Version: `{__python_version__}`\n"
     reply_msg += f"Pyrogram Version: `{__pyro_version__}`\n"
     reply_msg += f"Uptime: {uptime}"
-    reply_msg += "Support: [Click](https://t.me/TheGodfatherChat)"
-    reply_msg += "Channel: [Click](https://t.me/GodfatherUserBot)"
-    reply_msg += "[Powered By Godfather](https://github.com/Team-TGN/Godfather)"
+    reply_msg += "Support: [Click](https://t.me/TheGodfatherChat)\nChannel: [Click](https://t.me/GodfatherUserBot)\n\n[Powered By Godfather](https://github.com/Team-TGN/Godfather)"
     photo = "https://telegra.ph/file/2c564b0cd45f8e39ef7e2.jpg"
     await m.delete()
     if m.reply_to_message:
